bdb8ead9cf849f8f8de9103c0a63b278fa75042a
MaxiHuHe04 <maxiherczegh@outlook.com>
2022-04-18 21:25:59 +0200

    Added dependencies for multi-platform jars

---
+++ src/main/java/me/maxih/itunes_backup_explorer/ITunesBackupExplorerLauncher.java
@@ -0,0 +1,8 @@
+package me.maxih.itunes_backup_explorer;
+
+// Needed to provide a multi-platform jar
+public class ITunesBackupExplorerLauncher {
+	public static void main(String[] args) {
+		ITunesBackupExplorer.main(args);
+	}
+}
