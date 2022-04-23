d27c7b1922b514adbb903b5830f8469a2ba73d79
MaxiHuHe04 <maxiherczegh@outlook.com>
2022-04-23 23:43:26 +0200

    Fixed file extraction
    Added missing file size argument

--- src/main/java/me/maxih/itunes_backup_explorer/api/BackupFile.java
+++ src/main/java/me/maxih/itunes_backup_explorer/api/BackupFile.java
@@ -119,7 +119,7 @@ public class BackupFile {
                         throw new BackupReadException("Encrypted file in non-encrypted backup");
 
                     try {
-                        this.backup.manifest.getKeyBag().get().decryptFile(this.protectionClass, this.encryptionKey, this.contentFile, destination);
+                        this.backup.manifest.getKeyBag().get().decryptFile(this.protectionClass, this.encryptionKey, this.contentFile, destination, size);
                     } catch (InvalidKeyException e) {
                         throw new BackupReadException(e);
                     }
